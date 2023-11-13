1176a14a395aa388a1f23ac9a7ce16f1c2f18f39
Seungwon Jeong <75506995+Owenjeong@users.noreply.github.com>
2023-11-12 22:30:19 -0600

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -125,6 +125,10 @@ sorted_top30 = top30[['RegionName','lat', 'lon', 'Rate']]
 
 sorted_top30['Rate_str'] = (sorted_top30['Rate']*100).astype(str)
 
+st.dataframe(sorted_top30)
+st.write(len(soted_top30))
+
+
 view = pdk.ViewState(latitude=top30['lat'].mean(),
 longitude=top30['lon'].mean(),
 pitch = 50,
